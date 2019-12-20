0c49370fe216fd24b01afac81ab8ee8dfb56e463
David <davidoregan91@gmail.com>
2019-12-20 19:01:42 +0000

    feat(client): update base components for css

--- pages/index.js
+++ pages/index.js
@@ -1,88 +1,57 @@
 import React from 'react'
 import Link from 'next/link'
+import {
+  Button,
+  Container,
+  Divider,
+  Grid,
+  Header,
+  Icon,
+  Image,
+  List,
+  Menu,
+  Responsive,
+  Segment,
+  Sidebar,
+  Visibility,
+} from 'semantic-ui-react'
 import Layout from '../components/Layout'
 
 const Home = () => (
   <div>
     <Layout>
-      <div className="hero">
-        <h1 className="title">Welcome to Next!</h1>
-        <p className="description">
-          To get started, edit <code>pages/index.js</code> and save to reload.
-        </p>
-
-        <div className="row">
-          <Link href="https://github.com/zeit/next.js#getting-started">
-            <a className="card">
-              <h3>Getting Started &rarr;</h3>
-              <p>Learn more about Next on Github and in their examples</p>
-            </a>
-          </Link>
-          <Link href="https://open.segment.com/create-next-app">
-            <a className="card">
-              <h3>Examples &rarr;</h3>
-              <p>
-                Find other example boilerplates on the{' '}
-                <code>create-next-app</code> site
+      <Segment style={{ padding: '8em 0em' }} vertical>
+        <Grid container stackable verticalAlign='middle'>
+          <Grid.Row>
+            <Grid.Column width={8}>
+              <Header as='h3' style={{ fontSize: '2em' }}>
+                We Help Companies and Companions
+              </Header>
+              <p style={{ fontSize: '1.33em' }}>
+                We can give your company superpowers to do things that they never thought possible.
+                Let us delight your customers and empower your needs... through pure data analytics.
               </p>
-            </a>
-          </Link>
-          <Link href="https://github.com/segmentio/create-next-app">
-            <a className="card">
-              <h3>Create Next App &rarr;</h3>
-              <p>Was this tool helpful? Let us know how we can improve it</p>
-            </a>
-          </Link>
-        </div>
-      </div>
+              <Header as='h3' style={{ fontSize: '2em' }}>
+                We Make Bananas That Can Dance
+              </Header>
+              <p style={{ fontSize: '1.33em' }}>
+                Yes that's right, you thought it was the stuff of dreams, but even bananas can be
+                bioengineered.
+              </p>
+            </Grid.Column>
+            <Grid.Column floated='right' width={6}>
+              <Image bordered rounded size='large' src='/images/wireframe/white-image.png' />
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column textAlign='center'>
+              <Button size='huge'>Check Them Out</Button>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
     </Layout>
-
-    <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
-    `}</style>
+    <style jsx>{``}</style>
   </div>
 )
 

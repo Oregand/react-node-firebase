9ad17bc9e1df8ff426f5b38883682e93c30cd909
David <davidoregan91@gmail.com>
2019-12-20 19:34:14 +0000

    feat(client): update base components for css

--- pages/index.js
+++ pages/index.js
@@ -46,49 +46,49 @@ const Home = props => (
         </Grid>
       </Segment>
 
-    <Segment style={{ padding: '0em' }} vertical>
+      <Segment style={{ padding: '0em' }} vertical>
+        <Grid celled='internally' columns='equal' stackable>
+          <Grid.Row textAlign='center'>
+            <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
+              <Header as='h3' style={{ fontSize: '2em' }}>
+                "What a Company"
+              </Header>
+              <p style={{ fontSize: '1.33em' }}>That is what they all say about us</p>
+            </Grid.Column>
+            <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
+              <Header as='h3' style={{ fontSize: '2em' }}>
+                "I shouldn't have gone with their competitor."
+              </Header>
+              <p style={{ fontSize: '1.33em' }}>
+                <Image avatar src='/images/avatar/large/nan.jpg' />
+                <b>Nan</b> Chief Fun Officer Acme Toys
+              </p>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+      <Segment>
       <Grid celled='internally' columns='equal' stackable>
-        <Grid.Row textAlign='center'>
-          <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
-            <Header as='h3' style={{ fontSize: '2em' }}>
-              "What a Company"
-            </Header>
-            <p style={{ fontSize: '1.33em' }}>That is what they all say about us</p>
-          </Grid.Column>
-          <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
-            <Header as='h3' style={{ fontSize: '2em' }}>
-              "I shouldn't have gone with their competitor."
-            </Header>
-            <p style={{ fontSize: '1.33em' }}>
-              <Image avatar src='/images/avatar/large/nan.jpg' />
-              <b>Nan</b> Chief Fun Officer Acme Toys
-            </p>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    </Segment>
-    <Segment>
-    <Grid celled='internally' columns='equal' stackable>
-        <Grid.Row textAlign='center'>
-          <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
-            <Header as='h3' style={{ fontSize: '2em' }}>
-              Screams
-            </Header>
-            <List>
-              {props.screams.map(scream => (
-                scream ? (
-                  <List.Item>{scream.body}</List.Item>
-                )
-                : 
-                (
-                  <List.Item>No Screams here</List.Item>
-                )
-              ))}
-            </List>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    </Segment>
+          <Grid.Row textAlign='center'>
+            <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
+              <Header as='h3' style={{ fontSize: '2em' }}>
+                Screams
+              </Header>
+              <List>
+                {props.screams.map(scream => (
+                  scream ? (
+                    <List.Item>{scream.body}</List.Item>
+                  )
+                  : 
+                  (
+                    <List.Item>No Screams here</List.Item>
+                  )
+                ))}
+              </List>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
     </Layout>
     <style jsx>{``}</style>
   </div>
